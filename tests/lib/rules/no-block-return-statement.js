080b320e3fd274f439a1c88be10e40211789b6d3
Danny Michaelis <dmichaelis0@gmail.com>
2018-07-31 18:16:06 -0400

    working unit tests

--- tests/lib/rules/no-block-return-statement.js
+++ tests/lib/rules/no-block-return-statement.js
@@ -2,12 +2,14 @@ import { RuleTester } from 'eslint';
 
 import rule from '../../../lib/rules/no-block-return-statement';
 
-const ruleTester = new RuleTester();
+const ruleTester = new RuleTester({ parserOptions: { ecmaVersion: 2018 } }); // You do have to tell eslint what js you're using
 
 const errorObj = {
     message: 'No need for the `{`, `return` `}` syntax for an Arrow Function that only returns something.',
     type: 'ReturnStatement' // This may not be right
 };
+
+const isString = v => typeof v === 'string';
 
 ruleTester.run( 'no-block-return-statement', rule, {
     valid: [
@@ -15,25 +17,16 @@ ruleTester.run( 'no-block-return-statement', rule, {
         `const complex2 = s =>
             s.split('').join(' ').toUpperCase()`,
         'const capitalize2 = v => v.toUpperCase()'
-    ],
+    ].map( code => isString( code ) ? { code } : code ),
     invalid: [
-        {
-            code: `const ident = v => {
-                        return v
-                   }`,
-            errors: [ errorObj ]
-        },
-        {
-            code: `const capitalize = v => {
-                      return v.toUpperCase()
-                   }`,
-            errors: [ errorObj ]
-        },
-        {
-            code: `const complex = s => {
-                        return s.split('').join(' ').toUpperCase()
-                   } `,
-            errors: [ errorObj ]
-        }
-    ]
+        `const ident = v => {
+            return v
+        }`,
+        `const capitalize = v => {
+            return v.toUpperCase()
+        }`,
+        `const complex = s => {
+            return s.split('').join(' ').toUpperCase()
+        }`
+    ].map( code => isString( code ) ? { code, errors: [ errorObj ] } : code )
 } );
